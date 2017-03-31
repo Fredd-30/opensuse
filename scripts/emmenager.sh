f0c4b645f9df379c00fce0d6a8aafb5d121c6252
Niki Kovacs <info@microlinux.fr>
2017-03-31 10:10:29 +0200

    Fix.

--- scripts/emmenager.sh
+++ scripts/emmenager.sh
@@ -14,12 +14,17 @@ cat $CWD/../bash/root-bashrc > /root/.bashrc
 
 echo ":: Configuration de Bash pour les utilisateurs."
 cat $CWD/../bash/user-alias > /etc/skel/.alias
+for UTILISATEUR in $(ls /home); do
+  cat $CWD/../bash/user-alias > /home/$UTILISATEUR/.alias
+  chown $UTILISATEUR:users /home/$UTILISATEUR/.alias
+done
 
 # Xterm
 echo ":: Configuration de Xterm pour l'administrateur."
 cat $CWD/../xterm/Xresources > /root/.Xresources
 
 echo ":: Configuration de Xterm pour les utilisateurs."
+cat $CWD/../xterm/Xresources > /etc/skel/.Xresources
 for UTILISATEUR in $(ls /home); do
   cat $CWD/../xterm/Xresources > /home/$UTILISATEUR/.Xresouces
   chown $UTILISATEUR:users /home/$UTILISATEUR/.Xresources
