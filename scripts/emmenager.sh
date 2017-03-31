06d2acb03e7b6cf0c6552a7274a96b762dffae27
Niki Kovacs <info@microlinux.fr>
2017-03-31 09:53:37 +0200

    Xterm configuration.

--- scripts/emmenager.sh
+++ scripts/emmenager.sh
@@ -15,6 +15,16 @@ cat $CWD/../bash/root-bashrc > /root/.bashrc
 echo ":: Configuration de Bash pour les utilisateurs."
 cat $CWD/../bash/user-alias > /etc/skel/.alias
 
+# Xterm
+echo ":: Configuration de Xterm pour l'administrateur."
+cat $CWD/../xterm/Xresources > /root/.Xresources
+
+echo ":: Configuration de Xterm pour les utilisateurs."
+for UTILISATEUR in $(ls /home); do
+  cat $CWD/../xterm/Xresources > /home/$UTILISATEUR/.Xresouces
+  chown $UTILISATEUR:users /home/$UTILISATEUR/.Xresources
+done
+
 echo 
 
 exit 0
