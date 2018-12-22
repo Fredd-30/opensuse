e55f8cd2ad3994aa45e5c9f99e7cd0e910a5fcea
Nicolas Kovacs <info@microlinux.fr>
2018-12-22 07:39:36 +0100

    Finition du script.

--- scripts/06-personnaliser-menus.sh
+++ scripts/06-personnaliser-menus.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #
 # 06-personnaliser-menus.sh
+#
+# Nicolas Kovacs, 2019
+#
+# Ce script personnalise les entrées de menu KDE des applications en remplaçant
+# les fichiers *.desktop situés dans l'arborescence /usr/share/applications par
+# des versions modifiées. Les traductions sont fournies en français, en anglais
+# et en allemand. Dans le cas où une mise à jour écrase une entrée de menu
+# personnalisée, il suffit de réexécuter le script pour la restaurer.
 
 . source.sh
 
@@ -12,12 +20,12 @@ MENUDIRS="/usr/share/applications \
 echo
 
 for MENUDIR in $MENUDIRS; do
-	for ENTRY in $ENTRIES; do
-		if [ -r $MENUDIR/$ENTRY ]; then
-			echo ":: Configuration de l'entrée de menu $ENTRY."
-			cat $ENTRIESDIR/$ENTRY > $MENUDIR/$ENTRY
-		fi
-	done
+  for ENTRY in $ENTRIES; do
+    if [ -r $MENUDIR/$ENTRY ]; then
+      echo ":: Configuration de l'entrée de menu $ENTRY."
+      cat $ENTRIESDIR/$ENTRY > $MENUDIR/$ENTRY
+    fi
+  done
 done
 
 update-desktop-database 2> /dev/null
