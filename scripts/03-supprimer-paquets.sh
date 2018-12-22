9cb9ac665ebd32bc516737dd35bbfa6e9f2befe9
Nicolas Kovacs <info@microlinux.fr>
2018-12-22 07:29:07 +0100

    Finition du script.

--- scripts/03-supprimer-paquets.sh
+++ scripts/03-supprimer-paquets.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
 # 03-supprimer-paquets.sh
+#
+# Nicolas Kovacs, 2019
+#
+# Ce script supprime tous les paquets listés dans le fichier
+# paquets-inutiles.txt situé dans le répertoire ../config/pkglists. Il se
+# charge également de nettoyer les dépendances orphelines.
 
 . source.sh
 
